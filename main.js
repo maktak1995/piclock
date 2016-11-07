e3a83e28dc5215e3f376812936a1b4b971ed0f8c
MakTak <justaway22@gmail.com>
2016-11-07 13:57:17 +0900

    微修正

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow, Menu, Tray} = require('electron')
 const path = require('path')
 const url = require('url')
 
-var mainWindow
+let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -49,7 +49,7 @@ function createWindow () {
   tray.setContextMenu(menu)
 }
 
-app.on('ready', createWindow)
+app.on('ready',createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
