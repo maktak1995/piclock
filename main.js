2c33141a3e53f7a32433d0595c62d577203ee920
kou029w <kou029w@gmail.com>
2017-02-24 20:15:01 +0900

    linting

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const {app, BrowserWindow, Menu, Tray, ipcMain} = require('electron');
-const tw_oauth = require('node-twitter-api');
-const tw_client = require('twitter');
-const path = require('path');
-const url = require('url');
+const {app, BrowserWindow, Menu, Tray, ipcMain} = require('electron')
+const Oauth = require('node-twitter-api')
+const twClient = require('twitter')
+const path = require('path')
+const url = require('url')
 
-let mainWindow;
-let tray;
-let twitteroauthwindow;
-let client;
+let mainWindow
+let tray
+let twitteroauthwindow
+let client
 
-/*------------------Main function-------------------*/
+/* ------------------Main function------------------- */
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 200,
@@ -17,23 +17,23 @@ function createWindow () {
     resizable: false,
     transparent: true,
     frame: false
-  });
+  })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'src', 'scripts', 'index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 
-  tray = new Tray(path.join(__dirname, 'assets', 'appicon16.png'));
+  tray = new Tray(path.join(__dirname, 'assets', 'appicon16.png'))
 
   tray.on('click', () => {
-    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
-  });
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  })
 
   const menu = Menu.buildFromTemplate([
     {
@@ -46,50 +46,52 @@ function createWindow () {
       accelerator: 'CmdOrCtrl+M',
       type: 'checkbox',
       click: (item) => {
-        mainWindow.webContents.setAudioMuted(item.checked);
+        mainWindow.webContents.setAudioMuted(item.checked)
       }
     }
-  ]);
+  ])
 
-  Menu.setApplicationMenu(menu);
-  tray.setContextMenu(menu);
+  Menu.setApplicationMenu(menu)
+  tray.setContextMenu(menu)
 }
 
-
-/*-----------------Twitter function------------------*/
-const oauth = new tw_oauth({
-    consumerKey:    '74Ww7f88pRHLkmXYSKbqxO1xI',
-    consumerSecret: 'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
-    callback:       'http://127.0.0.1',
-});
+/* -----------------Twitter function------------------ */
+const oauth = new Oauth({
+  consumerKey: '74Ww7f88pRHLkmXYSKbqxO1xI',
+  consumerSecret: 'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
+  callback: 'http://127.0.0.1'
+})
 
 function twitteraouth () {
-  twitteroauthwindow = new BrowserWindow({width:400, height: 300,
-                        webPreferences: {webSecurity: false}});
+  twitteroauthwindow = new BrowserWindow({width: 400,
+    height: 300,
+    webPreferences: {webSecurity: false}})
   oauth.getRequestToken(function (error, requestToken, requestTokenSecret, results) {
-    var url = oauth.getAuthUrl(requestToken);
+    if (error) twitteroauthwindow.close()
+    var url = oauth.getAuthUrl(requestToken)
     twitteroauthwindow.webContents.on('will-navigate', function (event, url) {
-      var matched;
-      if (matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
+      var matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)
+      if (matched) {
         oauth.getAccessToken(requestToken, requestTokenSecret, matched[2], function (error, accessToken, accessTokenSecret, results) {
-          var twitter_accessToken = accessToken;
-          var twitter_accessTokenSecret = accessTokenSecret;
-          oauth.verifyCredentials(twitter_accessToken, twitter_accessTokenSecret, {}, function (error, data, respons) {
-            twitteroauthwindow.close();
-          });
+          if (error) twitteroauthwindow.close()
+          var twitterAccessToken = accessToken
+          var twitterAccessTokenSecret = accessTokenSecret
+          oauth.verifyCredentials(twitterAccessToken, twitterAccessTokenSecret, {}, function (_, data, respons) {
+            twitteroauthwindow.close()
+          })
 
-          client = tw_client({
-                      consumer_key:        '74Ww7f88pRHLkmXYSKbqxO1xI',
-                      consumer_secret:     'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
-                      access_token_key:    twitter_accessToken,
-                      access_token_secret: twitter_accessTokenSecret,
-                    });
-        });
+          client = twClient({
+            consumer_key: '74Ww7f88pRHLkmXYSKbqxO1xI',
+            consumer_secret: 'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
+            access_token_key: twitterAccessToken,
+            access_token_secret: twitterAccessTokenSecret
+          })
+        })
       }
-      event.preventDefault();
-    });
-    twitteroauthwindow.loadURL(url);
-  });
+      event.preventDefault()
+    })
+    twitteroauthwindow.loadURL(url)
+  })
 }
 
 let mentionText
@@ -97,27 +99,27 @@ ipcMain.on('mention', (event, arg) => {
   if (client == null) return
 
   client.get('statuses/mentions_timeline', (error, tweet, response) => {
-    if (!error && mentionText != tweet[0]['text']) {
+    if (!error && mentionText !== tweet[0]['text']) {
       mentionText = tweet[0]['text']
       event.sender.send('mention', mentionText)
     }
   })
 })
 
-/*---------Main process----------*/
+/* ---------Main process---------- */
 app.on('ready', () => {
-  twitteraouth();
-  createWindow();
-});
+  twitteraouth()
+  createWindow()
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow == null) {
-    createWindow();
+    createWindow()
   }
-});
+})
--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,77 +1,86 @@
-/*--
+/* --
 FILE :input.js
 ABOUT:input serif, picture, voice
 AUTHER:MakTak
 UPDATE:2016/11/5
---*/
-/*-----------------------画像セット-----------------------*/
-var i = 0;
-var img = new Array();
-img[0] = new Image();
-img[0].src = "../img/main.png";
-for(i=1;i<=10;i++){
-  img[i] = new Image();
-  img[i].src = "../img/sub" + String(i) + ".png";
+-- */
+
+/* -----------------------画像セット----------------------- */
+var i = 0
+var img = []
+img[0] = document.createElement('img')
+img[0].src = '../img/main.png'
+for (i = 1; i <= 10; i++) {
+  img[i] = document.createElement('img')
+  img[i].src = '../img/sub' + String(i) + '.png'
 }
 
-/*-----------------------音声セット-----------------------*/
+/* -----------------------音声セット----------------------- */
 
-var v_text = new Array();
-for(i=0;i<=6;i++){
-  v_text[i] = new Audio();
-  v_text[i].src = "../voice/text/text" + String(i) + ".wav";
+var vText = []
+for (i = 0; i <= 6; i++) {
+  vText[i] = document.createElement('audio')
+  vText[i].src = '../voice/text/text' + String(i) + '.wav'
 }
 
-var v_reaction = new Array();
-for(i=0;i<=3;i++){
-  v_reaction[i] = new Audio();
-  v_reaction[i].src = "../voice/reaction/reaction" + String(i) + ".wav";
+var vReaction = []
+for (i = 0; i <= 3; i++) {
+  vReaction[i] = document.createElement('audio')
+  vReaction[i].src = '../voice/reaction/reaction' + String(i) + '.wav'
 }
 
-var v_zihou = new Array();
-for(i=0;i<=23;i++){
-  v_zihou[i] = new Audio();
-  v_zihou[i].src = "../voice/zihou/zihou" + String(i) + ".wav";
+var vJihou = []
+for (i = 0; i <= 23; i++) {
+  vJihou[i] = document.createElement('audio')
+  vJihou[i].src = '../voice/zihou/zihou' + String(i) + '.wav'
 }
 
-/*----------------------セリフセット----------------------*/
-var text = new Array();//通常
-text[0] = 'ハロー。';//main
-text[1] = 'マスター、暇です。';//sub4
-text[2] = '今何時？上見てくださいよ。';//sub6
-text[3] = 'ゲームとかしません？';//sub5
-text[4] = '忙しそうですね。';//sub10
-text[5] = 'コーヒーでもいかがです?';//sub1
-text[6] = 'あなたのお友達からメンションですよ。';//mention_tweet
+/* ----------------------セリフセット---------------------- */
+var text = []// 通常
+text[0] = 'ハロー。'// main
+text[1] = 'マスター、暇です。'// sub4
+text[2] = '今何時？上見てくださいよ。'// sub6
+text[3] = 'ゲームとかしません？'// sub5
+text[4] = '忙しそうですね。'// sub10
+text[5] = 'コーヒーでもいかがです?'// sub1
+text[6] = 'あなたのお友達からメンションですよ。'// mention_tweet
 
-var zihou = new Array(); //時報
-zihou[0] = '0時です。日付変わりましたよ。もう寝たらいかがです?';//sub3
-zihou[1] = '1時です。頑張りますねえ。';//main
-zihou[2] = '2時です。しょうがないですね。私も付き合いますよ。';//sub1
-zihou[3] = '3時です。丑三つ時は幽霊がこわいですよね。';//sub7
-zihou[4] = '4時です。そろそろ休憩しません?';//sub9
-zihou[5] = '5時です。徹夜でお疲れ様ですね。';//sub1
-zihou[6] = '6時です。もう日が昇りましたよ。うおっまぶしっ';//sub6
-zihou[7] = '7時です。そろそろおなかがすきました...';//sub8
-zihou[8] = '8時です。朝ごはんを食べましょう!';//sub2
-zihou[9] = '9時です。そろそろ出発の支度をしましょう。';//sub9
-zihou[10] = '10時です。...とくに言うこともないです。';//sub6
-zihou[11] = '11時です。おや、むこうからいいにおいが...';//sub10
-zihou[12] = '12時です。お昼ですよー!';//sub2
-zihou[13] = '13時です。お昼食べた後はなんだか眠くなります。';//sub8
-zihou[14] = '14時です。暇つぶしにゲームしませんか?';//main
-zihou[15] = '15時です。おやつです! おやつです!';//sub2
-zihou[16] = '16時です。このボスを倒せば全クリです...!';//sub10
-zihou[17] = '17時です。ふふ、私にかかればこんなものですよ。';//sub5
-zihou[18] = '18時です。今日も一日お疲れ様でした。';//sub9
-zihou[19] = '19時です。今日の晩ごはんは何にしましょうか?';//sub10
-zihou[20] = '20時です。腹ごなしにゲームなどいかがです?';//sub5
-zihou[21] = '21時です。くそぅ...マスター強いですね...';//sub4
-zihou[22] = '22時です。そろそろよいこは寝る時間ですね。';//sub1
-zihou[23] = '23時です。早く寝ないと、体に毒ですよ？';//sub7
+var jihou = [] // 時報
+jihou[0] = '0時です。日付変わりましたよ。もう寝たらいかがです?'// sub3
+jihou[1] = '1時です。頑張りますねえ。'// main
+jihou[2] = '2時です。しょうがないですね。私も付き合いますよ。'// sub1
+jihou[3] = '3時です。丑三つ時は幽霊がこわいですよね。'// sub7
+jihou[4] = '4時です。そろそろ休憩しません?'// sub9
+jihou[5] = '5時です。徹夜でお疲れ様ですね。'// sub1
+jihou[6] = '6時です。もう日が昇りましたよ。うおっまぶしっ'// sub6
+jihou[7] = '7時です。そろそろおなかがすきました...'// sub8
+jihou[8] = '8時です。朝ごはんを食べましょう!'// sub2
+jihou[9] = '9時です。そろそろ出発の支度をしましょう。'// sub9
+jihou[10] = '10時です。...とくに言うこともないです。'// sub6
+jihou[11] = '11時です。おや、むこうからいいにおいが...'// sub10
+jihou[12] = '12時です。お昼ですよー!'// sub2
+jihou[13] = '13時です。お昼食べた後はなんだか眠くなります。'// sub8
+jihou[14] = '14時です。暇つぶしにゲームしませんか?'// main
+jihou[15] = '15時です。おやつです! おやつです!'// sub2
+jihou[16] = '16時です。このボスを倒せば全クリです...!'// sub10
+jihou[17] = '17時です。ふふ、私にかかればこんなものですよ。'// sub5
+jihou[18] = '18時です。今日も一日お疲れ様でした。'// sub9
+jihou[19] = '19時です。今日の晩ごはんは何にしましょうか?'// sub10
+jihou[20] = '20時です。腹ごなしにゲームなどいかがです?'// sub5
+jihou[21] = '21時です。くそぅ...マスター強いですね...'// sub4
+jihou[22] = '22時です。そろそろよいこは寝る時間ですね。'// sub1
+jihou[23] = '23時です。早く寝ないと、体に毒ですよ？'// sub7
 
-var reaction = new Array();
-reaction[0] = '私の顔になにかついてます?';//sub6
-reaction[1] = 'どうかしましたか?';//sub9
-reaction[2] = 'ゲームの邪魔しないでもらえます?';//sub3
-reaction[3] = '仕事してください。';//sub8
+var reaction = []
+reaction[0] = '私の顔になにかついてます?'// sub6
+reaction[1] = 'どうかしましたか?'// sub9
+reaction[2] = 'ゲームの邪魔しないでもらえます?'// sub3
+reaction[3] = '仕事してください。'// sub8
+
+exports.img = img
+exports.vText = vText
+exports.vReaction = vReaction
+exports.vJihou = vJihou
+exports.text = text
+exports.reaction = reaction
+exports.jihou = jihou
