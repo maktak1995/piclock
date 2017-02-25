2950c219d3c6e22f855e08bd92090b45d8298cbd
MakTak <justaway22@gmail.com>
2017-02-25 18:06:53 +0900

    Pomodoroを2重でセットした場合の対処を追加

--- src/scripts/avator.js
+++ src/scripts/avator.js
@@ -129,11 +129,14 @@ ipcRenderer.on('mention', (event, mention) => {
 ipcRenderer.send('ready')
 
 /* ----------------pomodoro function--------------- */
+let PomoID;
+
 ipcRenderer.on('pomodoro', (event, message) => {
   document.getElementById('yukarin_serif').innerHTML = text[7]
   changeIMG(1)
   playvoice('text', 7)
-  setTimeout(countPomodoro, 1500000) // 1500000ms = 25min
+  clearTimeout(PomoID); //既に動いているタイマーを(あれば)停止しておく
+  PomoID=setTimeout(countPomodoro, 1500000) // 1500000ms = 25min
 })
 
 function countPomodoro () {
