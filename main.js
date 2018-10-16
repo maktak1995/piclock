619fbe31fa9bd8bc2b09cf9f8f54243713ade2a3
MakTak <justaway22@gmail.com>
2018-10-16 12:16:53 +0900

    twitter連携をオミット

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const {app, BrowserWindow, Menu, Tray, ipcMain} = require('electron')
-const Oauth = require('node-twitter-api')
-const twClient = require('twitter')
 const path = require('path')
 const url = require('url')
 
 let mainWindow
 let tray
-let twitteroauthwindow
 
 /* ------------------Main function------------------- */
 function createWindow () {
@@ -56,75 +53,12 @@ function createWindow () {
       click: () => {
         mainWindow.webContents.send('pomodoro')
       }
-    },
-    {
-      label: 'Todolist',
-      accelerator: 'CmdOrCtrl+T',
-      type: 'checkbox',
-      click: (item) => {
-        mainWindow.webContents.send('todolist', [item.checked, mainWindow.getPosition()])
-      }
     }
   ])
 
   Menu.setApplicationMenu(menu)
   tray.setContextMenu(menu)
 }
-
-/* -----------------Twitter function------------------ */
-
-ipcMain.once('ready', (event, arg) => {
-  twitteroauthwindow = new BrowserWindow({
-    width: 400,
-    height: 300,
-    webPreferences: {webSecurity: false}
-  })
-
-  const oauth = new Oauth({
-    consumerKey: '74Ww7f88pRHLkmXYSKbqxO1xI',
-    consumerSecret: 'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
-    callback: 'http://127.0.0.1'
-  })
-
-  oauth.getRequestToken(function (error, requestToken, requestTokenSecret, results) {
-    if (error) twitteroauthwindow.close()
-
-    twitteroauthwindow.loadURL(oauth.getAuthUrl(requestToken))
-
-    twitteroauthwindow.webContents.on('will-navigate', function (_, url) {
-      const matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)
-
-      if (matched) {
-        oauth.getAccessToken(requestToken, requestTokenSecret, matched[2], (error, accessToken, accessTokenSecret, results) => {
-          if (error) twitteroauthwindow.close()
-
-          oauth.verifyCredentials(accessToken, accessTokenSecret, {}, (error, data, respons) => {
-            if (!error) {
-              const client = twClient({
-                consumer_key: '74Ww7f88pRHLkmXYSKbqxO1xI',
-                consumer_secret: 'f6tpMmCwxYXtl7Qn6eMWXVviOHOQLHVp1pdivIhQ4pNw6SalPr',
-                access_token_key: accessToken,
-                access_token_secret: accessTokenSecret
-              })
-
-              const stream = client.stream('statuses/filter', {track: `@${data.screen_name}`})
-
-              stream.on('data', function (data) {
-                event.sender.send('mention', data && data.text)
-              })
-
-              stream.on('error', function (error) {
-                throw error
-              })
-            }
-            twitteroauthwindow.close()
-          })
-        })
-      }
-      event.preventDefault()
-    })
-  })
-})
 
 /* ---------Main process---------- */
 app.on('ready', () => {
